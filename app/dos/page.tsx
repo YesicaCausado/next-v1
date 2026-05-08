e2b43e5728e1abb728d87e8478a7bbe9253c71c8
YesicaCausado <Yesica.causadoa@uniagustiniana.edu.co>
2026-05-08 18:58:25 +0000

    clase 8/05

---
+++ app/dos/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Hijo from "./hijo";
+export default function Dos() {
+  return (
+    <div>
+        <hijo>
+            dos
+        </hijo>    
+    </div>
+  );
+}
---
+++ app/uno/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Props from "./props";
+export default function Uno() {
+  return (
+    <div>
+        uno 
+        <Props nombre = "este es un props"></Props>
+      
+    </div>
+  );
+}
